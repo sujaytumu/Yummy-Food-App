8faaaae38a44036fe8f0676f298ccfc6d913c215
sujayyy <150987692+sujaytumu@users.noreply.github.com>
2025-07-08 18:54:19 +0530

    Update index.js for deploy

--- Backend/index.js
+++ Backend/index.js
@@ -1,44 +1,45 @@
 const express = require("express");
 const dotEnv = require('dotenv');
 const mongoose = require('mongoose');
-const vendorRoutes = require('./routes/vendorRoutes');
 const bodyParser = require('body-parser');
-const firmRoutes = require('./routes/firmRoutes');
-const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
 const path = require('path');
 
-const app = express()
+const vendorRoutes = require('./routes/vendorRoutes');
+const firmRoutes = require('./routes/firmRoutes');
+const productRoutes = require('./routes/productRoutes');
 
+
+const app = express()
 const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
 
-// Add before other routes
+//Middlewares
+app.use(cors())
+app.use(bodyParser.json());
+
+//mongo DB connection
+mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected successfully!"))
+    .catch((error) => console.log(error))
+
+// API Routes
+app.use('/vendor', vendorRoutes);
+app.use('/firm', firmRoutes)
+app.use('/product', productRoutes);
+app.use('/uploads', express.static('uploads'));
+
+// React Frontend (Customer)
 app.use(express.static(path.join(__dirname, "../Customer_Frontend/build")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../Customer_Frontend/build", "index.html"));
 });
 
-//
-app.use(cors())
-
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB connected successfully!"))
-    .catch((error) => console.log(error))
-
-app.use(bodyParser.json());
-app.use('/vendor', vendorRoutes);
-app.use('/firm', firmRoutes)
-app.use('/product', productRoutes);
-app.use('/uploads', express.static('uploads'));
-
+// Start server
 app.listen(PORT, () => {
     console.log(`server started and running at ${PORT}`);
 });
-
-app.use('/', (req, res) => {
-    res.send("<h1> Welcome to Yummy App");
-})
 
